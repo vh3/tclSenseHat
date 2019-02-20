2ae632666ad68b57cc8bab66877dc8f4f49bb236
vh3 <34223414+vh3@users.noreply.github.com>
2019-02-19 23:51:01 -0500

    Add files via upload

--- ex2.tcl
+++ ex2.tcl
@@ -1,28 +1,31 @@
-# ex2.tcl - scripts to display 'full screen' colour rainbows.
+# ex2.tcl - scripts to display 'full screen' colour rainbows and other graphics
 # vh, 10 May , 2017
 # --------------------------------------------------------------------
 
 source device.tcl
-set device(gif_output) "colours1"
-package require Tk
-wm withdraw .
+# package require Tk
+# wm withdraw .
 
+# Use the fb2gif to dump the contents of the frame buffer to a file, then use the gif_assemble command to put them together in an animated .gif file
+
+# EXAMPLE 1 - rainbow colours wiping by rotation
 # Play around with the pixel rendering.  options: circle, circle1...circle4, bear, heart, diagonal, vertical, plus or any letter of the alphabet
-set device(pixel_render) "diagonal"
-set device(gif_output) "colours_diagonal.gif"
+set device(gif_output) "colours1"
+#set device(pixel_render) "diagonal"
+# set device(gif_output) "colours_diagonal.gif"
 # Display shifting rainbow colours
-for {set angle -90} {$angle < 270} {incr angle} {
-	
-	set h [colour_transition $angle]
-	set_pixel_list $h
-	fb2gif
- }
- gif_assemble
- exit
+#for {set angle -90} {$angle < 270} {incr angle} {
+#	
+#	set h [colour_transition $angle]
+#	set_pixel_list $h
+#	#fb2gif
+# }
+# gif_assemble
+# exit
 
+# EXAMPLE 2 - rainbow colours wiping by shifting left
 wipe
-
-set device(gif_output) "colours2"
+# set device(gif_output) "colours2"
 # Create the startup list of hue values and draw them
 set h [colour_transition 23.5]
 set_pixel_list $h
@@ -35,9 +38,8 @@ for {set counter 0} {$counter <=1.0} {set counter [expr $counter + 0.01]} {
 
 	incrlist h 0.01
 	set_pixel_list $h
-	fb2gif
+	#fb2gif
 }
 
- gif_assemble
-
-return
+# gif_assemble
+exit
